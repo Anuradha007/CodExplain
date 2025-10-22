a607fa3000815d80b5da89796db7acb8f7208ed4
Anuradha Raj <shikhhaayadav@gmail.com>
2025-10-23 00:08:22 +0530

    create explain-code REST endpoint

--- server/server.js
+++ server/server.js
@@ -23,4 +23,16 @@
 
  app.use(express.json({ limit: "10mb" }));
 
- 
+ app.post()
+ app.post("/api/explain-code", async (req, res)=> {
+    try {
+     const { code, language } = req. body;
+     if(!code) {   
+        return res.status(400).json({ error: "Code is required" })
+     }
+    } catch (error) {  
+        console.error("Code Explain API error:", err);
+        res.status(500).json({ error: "Server error", details: err.message });
+    }
+
+ })
