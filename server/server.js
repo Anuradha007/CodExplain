691251969e57597b88bb68040607a591a4ff88a9
Anuradha Raj <shikhhaayadav@gmail.com>
2025-10-23 00:39:20 +0530

    Integrate LLM with API

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@
  import cors from "cors";
  import rateLimit from "express-rate-limit";
  import helmet from "helmet";
+ import { OpenAI } from "openai/client.js";
 
  const app = express();
 
@@ -23,16 +24,54 @@
 
  app.use(express.json({ limit: "10mb" }));
 
- app.post()
+ const API_KEY = process.env.NEBIUS_API_KEY;
+
+ const client = new OpenAI({
+    baseURL: "https://api.studio.nebius.com/v1/",
+    apiKey: API_KEY,
+ });
+
  app.post("/api/explain-code", async (req, res)=> {
     try {
      const { code, language } = req. body;
      if(!code) {   
         return res.status(400).json({ error: "Code is required" })
      }
+
+     const messages = [
+      {
+        role: "user",
+        content: `Please explain this ${
+          language || ""
+        } code in simple terms:\n\n\`\`\`${language || ""}\n${code}\n\`\`\``,
+      },
+    ];
+
+    const response = await client.chat.completions.create({
+        model:"openai/gpt-oss-120b",
+        messages,
+        temperature: 0.3,
+        max_tokens: 800,
+    });
+
+    const explaination = response?.choices[0]?.message?.content;
+
+    if(!explaination) {
+        return res.status(500).json({ error: "Failed to explain code" });
+    }
+
+    res.json({ explaination, language: language || "unknown" });
+
     } catch (error) {  
         console.error("Code Explain API error:", err);
         res.status(500).json({ error: "Server error", details: err.message });
     }
 
+ });
+
+ const PORT = process.env.PORT || 3002;
+
+    
+ app.listen(PORT, () => {
+    console.log(`API server listening to http://localhost:${PORT}`) 
  })
