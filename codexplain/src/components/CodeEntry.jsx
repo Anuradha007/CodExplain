d63e3a383d9c8f16ed8e39c32e60aabaa258c93c
Anuradha Raj <shikhhaayadav@gmail.com>
2025-10-23 01:58:06 +0530

    React with vite

---
+++ codexplain/src/components/CodeEntry.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header"
+
+  
+ const CodeEntry = () => {
+   return (
+     <div className="min-h-screen flex flex-col items-center p-6" >
+        <Header />
+     </div>
+   )
+ }
+ 
+ export default CodeEntry
